71694edb3285a5fb268abe8ab16e4c69f31a3758
Jeonghyeon Yang <36444454+yxzzzxh@users.noreply.github.com>
2021-11-01 15:54:05 +0900

    [feat] Implement to install helm chart (#54)
    - using go-helm-client package
    - add test code
    - upgrade CRD kubebuilder version

--- util/helmclient/client.go
+++ util/helmclient/client.go
@@ -1,36 +1,43 @@
 package helmclient
 
-/*
-type Client struct {
-    helmclient *gohelm.HelmClient
-}
-*/
+import (
+	"context"
 
-/*
+	gohelm "github.com/mittwald/go-helm-client"
+)
+
 type Client struct {
-    helmclient *gohelm.HelmClient
+	client gohelm.Client
 }
-*/
 
 // InstallChart installs helm chart
-func InstallChart(releaseName, chartDir string) error {
-	/*
-		// Use an unpacked(locally cloned) chart directory
-		chartSpec := gohelm.ChartSpec{
-			ReleaseName: releaseName,
-			ChartName:   chartDir,
-			Namespace:   "default",
-			UpgradeCRDs: true,
-			Wait:        true,
-		}
+func (c *Client) InstallChart(releaseName, chartDir, namespace string) error {
+	// Use an unpacked(locally cloned) chart directory
+	chartSpec := gohelm.ChartSpec{
+		ReleaseName: releaseName,
+		ChartName:   chartDir,
+		Namespace:   namespace,
+		UpgradeCRDs: true,
+		Wait:        true,
+	}
 
-		_, err := c.helmclient.InstallOrUpgradeChart(context.Background(), &chartSpec)
+	// If the chart is already installed, trigger an upgrade instead.
+	_, err := c.client.InstallOrUpgradeChart(context.Background(), &chartSpec)
 
+	if err != context.DeadlineExceeded { // TODO 확인필요. 리소스들도 정상적으로 다 생기는데, 왜 이게 발생하는 걸까?
 		if err != nil {
 			panic(err)
 		}
+	}
 
-		return err
-	*/
+	return nil
+}
+
+// UninstallReleaseByName uninstalls a release identified by the provided 'name'.
+func (c *Client) UninstallReleaseByName(releaseName string) error {
+	if err := c.client.UninstallReleaseByName(releaseName); err != nil {
+		panic(err)
+	}
+
 	return nil
 }
