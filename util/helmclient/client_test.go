fd0eb679d38023ba486419bd7d5cc611c63e498d
Jeonghyeon Yang <yxzzzxh@gmail.com>
2021-11-10 10:29:02 +0900

    [feat] Implement InstallChartByCLI and UninstallReleaseByCLI

--- util/helmclient/client_test.go
+++ util/helmclient/client_test.go
@@ -43,7 +43,7 @@ func TestInstallChart(t *testing.T) {
 	// 2. 로컬에 저장된 경로를 이용하여 chart install
 	releaseName := "test-" + randomString
 	chartPath := path + "/helm-guestbook"
-	namespace := "default"
+	namespace := "test-1109"
 	chartSpec := &gohelm.ChartSpec{
 		ReleaseName: releaseName,
 		ChartName:   chartPath,
@@ -72,8 +72,66 @@ func TestInstallChart(t *testing.T) {
 		panic(err.Error())
 	}
 
-	deploy, _ := clientset.AppsV1().Deployments("default").Get(context.Background(), releaseName+"-helm-guestbook", v1.GetOptions{})
-	svc, _ := clientset.CoreV1().Services("default").Get(context.Background(), releaseName+"-helm-guestbook", v1.GetOptions{})
+	deploy, err := clientset.AppsV1().Deployments("default").Get(context.Background(), releaseName+"-helm-guestbook", v1.GetOptions{})
+	assert.Equal(t, err, nil)
+	svc, err := clientset.CoreV1().Services("default").Get(context.Background(), releaseName+"-helm-guestbook", v1.GetOptions{})
+	assert.Equal(t, err, nil)
+
+	assert.NotEqual(t, deploy, nil)
+	assert.NotEqual(t, svc, nil)
+}
+
+func TestInstallChartByCLI(t *testing.T) {
+	// TODO : 네임스페이스가 없는 경우도 테스트 코드 짤 것 (INSTALLATION FAILED: create: failed to create: namespaces "testtesttest" not found)
+	testHelmClient := &Client{}
+
+	url := "https://github.com/tmax-cloud/cd-example-apps"
+	randomString := utils.RandomString(5)
+	path := "/tmp/test-" + randomString
+	revision := "main"
+
+	defer os.RemoveAll(path)
+
+	// 1. 로컬에 helm manifest의 git repo clone
+	err := gitclient.Clone(url, path, revision)
+	assert.Equal(t, err, nil)
+
+	// 2. 로컬에 저장된 경로를 이용하여 chart install
+	releaseName := "test-" + randomString
+	chartPath := path + "/helm-guestbook"
+	namespace := "testjh"
+	chartSpec := &gohelm.ChartSpec{
+		ReleaseName: releaseName,
+		ChartName:   chartPath,
+		Namespace:   namespace,
+		UpgradeCRDs: true,
+		Wait:        false,
+	}
+
+	err = testHelmClient.InstallChartByCLI(chartSpec)
+	assert.Equal(t, err, nil)
+
+	defer func() {
+		err := testHelmClient.UninstallReleaseByCLI(releaseName, namespace)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	cfg, err := config.GetConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	deploy, err := clientset.AppsV1().Deployments("testjh").Get(context.Background(), releaseName+"-helm-guestbook", v1.GetOptions{})
+	assert.Equal(t, err, nil)
+	svc, err := clientset.CoreV1().Services("testjh").Get(context.Background(), releaseName+"-helm-guestbook", v1.GetOptions{})
+	assert.Equal(t, err, nil)
 
 	assert.NotEqual(t, deploy, nil)
 	assert.NotEqual(t, svc, nil)
